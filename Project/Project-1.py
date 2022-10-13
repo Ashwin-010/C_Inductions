f62621146631e6a3d07d24b592f0945d178ee0eb
Pranav <109609514+Pranav-020@users.noreply.github.com>
2022-10-13 16:46:28 +0530

    Timetable for 9th included
    1)Added option to create timetable for 9th
    2)Changed everything to dictionary for more efficiency(Same as previous commit)
    3)Have added keys for multiple keys in the main dictionary but haven't tried with conflicting teacher timetables or conflicting class schedules
    4)Have switched to numbers instead of roman numerals since dictionary is used and not a variable

--- Project/Project-1.py
+++ Project/Project-1.py
@@ -7,7 +7,7 @@ def info():
         sub = input("Enter subject name:")
         teacher = input("Enter teacher name:")
         nump = int(input("Enter the number of periods:"))
-        tecclass = input("Enter the class which the teacher handles(Enter in roman numerals:")
+        tecclass = input("Enter the class which the teacher handles:")
         yorn = input("Do you want to add more records?(y/n):")
         w.writerow([sub,teacher,nump,tecclass])
         if yorn == 'n':
@@ -21,18 +21,33 @@ def lcreation():
     w.writerow(['Day','1','2','3','4','5','6','7','8','9'])
     r = csv.reader(f)
     l1 = list(r)
-    d1 = {'XE':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'XF':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'XG':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'XH':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'XJ':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']]}
+    d9 = {'9E':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'9F':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'9G':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'9H':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'9J':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']]}
+    d10 = {'10E':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'10F':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'10G':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'10H':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'10J':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']]}
     j = 0
     for i in range(1,len(l1)):
         k = 0
         while k < int(l1[i][2]):
             l = l1[i][3]
-            d1[l][j].append(l1[i][0])
+            if '9' in l:
+                d9[l][j].append(l1[i][0])
+            if '10' in l:
+                d10[l][j].append(l1[i][0])
             j += 1
             if j > 4:
                 j = 0
             k += 1
-    print(d1)
+    for i in d9:
+        d9[i].insert(0,[i])
+    for i in d10:
+        d10[i].insert(0,[i])
+    for i in d9:
+        w.writerows(d9[i])
+        w.writerow([])
+    for i in d10:
+        w.writerows(d10[i])
+        w.writerow([])
+    print(d9)
+    print(d10)
     f.close()
     g.close()
             
