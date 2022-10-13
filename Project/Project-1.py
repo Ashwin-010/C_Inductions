104dcc8de74cc39cef33dc4c1c38d10a38ee6f62
Pranav <109609514+Pranav-020@users.noreply.github.com>
2022-10-13 06:59:10 +0530

    Option for classes in csv
    Added classes option in csv file and created a dictionary for each class.
    Yet to do-1)Create records for multiple class and teachers into data.csv file 
    2)Write the timetable for each class into csv
    3)Try with a single teacher handling multiple classes

--- Project/Project-1.py
+++ Project/Project-1.py
@@ -2,13 +2,14 @@ import csv
 def info():
     f = open('data.csv','w+',newline = '')
     w = csv.writer(f)
-    w.writerow(['Subject','Teacher','Number of periods'])
+    w.writerow(['Subject','Teacher','Number of periods','Class'])
     while True:
         sub = input("Enter subject name:")
         teacher = input("Enter teacher name:")
         nump = int(input("Enter the number of periods:"))
+        tecclass = input("Enter the class which the teacher handles(Enter in roman numerals:")
         yorn = input("Do you want to add more records?(y/n):")
-        w.writerow([sub,teacher,nump])
+        w.writerow([sub,teacher,nump,tecclass])
         if yorn == 'n':
             break
     f.close()
@@ -20,21 +21,21 @@ def lcreation():
     w.writerow(['Day','1','2','3','4','5','6','7','8','9'])
     r = csv.reader(f)
     l1 = list(r)
-    l = [['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']]
+    d1 = {'XE':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'XF':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'XG':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'XH':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']],'XJ':[['Monday'],['Tuesday'],['Wednesday'],['Thursday'],['Friday']]}
     j = 0
     for i in range(1,len(l1)):
         k = 0
         while k < int(l1[i][2]):
-            l[j].append(l1[i][0])
+            l = l1[i][3]
+            d1[l][j].append(l1[i][0])
             j += 1
             if j > 4:
                 j = 0
             k += 1
-    w.writerows(l)
+    print(d1)
     f.close()
     g.close()
             
 lcreation()
-    
     
             
