7b074af279d9ee15b41d0bed661e02f0577c6ecb
Pranav <109609514+Pranav-020@users.noreply.github.com>
2022-11-02 17:05:34 +0530

    Calculate avg
    Calculates average and asks user to choose a restaurant for which a menu is display. Formatting yet to be done

--- Project New/Python File/Code.py
+++ Project New/Python File/Code.py
@@ -1,19 +1,4 @@
 import csv
-
-def userinput():#Todo Add an order view option(More efficient to do this maybe after implementation of GUI)
-    cart = []#List containing items ordered
-    while True:
-        food = input("What would you like to order:")
-        cart.append(food)
-        yorn = input("Would you like to add another item to your cart?(Y/N):")#CLI version of adding items to cart
-        if yorn.lower() == "n":
-            break
-    print(cart)
-    f = open('Order.csv','w')#Text file containing all the products ordered(Created for future use when they want to view order)
-    w = csv.writer(f)
-    w.writerow(cart)
-    return cart
-
 def getdata():#Function to get the data of the restaurants from a csv file
     f = open('data.csv','r')
     r = csv.reader(f)
@@ -32,48 +17,31 @@ def getdata():#Function to get the data of the restaurants from a csv file
     return d1
 
 
-def finditem(userfood):#Function to create a list with the item requested by the user in all restaurants
-    foodinrest = []#List to be created -> [[Restaurant Name1, Food required, Price],[Res Name2, Food required,Price]]
-    for i in restdict:#Finding the item name in a particular restaurant from the dict(Values=Nested List) created in above fn using binary search
-        l1 = restdict[i]
-        l1.sort()
-        L = 0
-        U = len(l1)-1
-        while L <= U:
-            mid = (L+U)//2
-            if userfood.lower() == l1[mid][0].lower():
-                foodneeded = l1[mid]
-                foodneeded.insert(0,i)
-                foodinrest.append(foodneeded)
-                break
-            elif userfood > l1[mid][0]:
-                L = mid+1
-            elif userfood < l1[mid][0]:
-                U = mid-1
-    return foodinrest
+def cheaprest(restdict):#function to find cheapest priced restaurant
+    lowprice = ('a',1000000000000000000000000000000000000000)
+    averres = []#List containing the prices of a particular restaurant
+    for i in restdict:
+        menu = restdict[i]
+        price = []
+        for items in menu:#Iteration to append price of a particular restaurant to the list
+            itemprice = int(items[-1])
+            price.append(itemprice)
+        sumprices = sum(price)
+        average = sumprices/len(price)
+        averres.append((i,average))#Creates a tuple in averres = [(Restaurant Name, Average)]
+    return averres
 
-def lowestprice(userfoodinrests):#Find the lowest price from the list created by the function finditem(userfood)
-    lowestprice = [['','','1000000000000000000000000000000000000000000000000000000000000']]
-    for i in userfoodinrests:
-        prevres = lowestprice[0][-1]#Value stored in lowestprice before
-        newres = i[-1]#New value in the list
-        if int(prevres) > int(newres):#If the given number is lesser than the one before we equate this in that list
-            lowestprice[0] = i
-        elif int(prevres) == int(newres):
-            lowestprice.append(i)
-    if len(lowestprice) == 2:#A case where if two restaurants have the same food at the same price
-        for i in range(len(lowestprice)):
-            if i == 0:
-                print(userfood,"is available at the best price at",lowestprice[0][0],",","Cost =",lowestprice[0][2])
-            if i == 1:
-                print(userfood, "is also available at the best price at",lowestprice[1][0],",","Cost =", lowestprice[1][2])
-    else:
-        print(userfood, "is available at the best price at", lowestprice[0][0],",","Cost =", lowestprice[0][2])
+def dispavg(averrest,restdict):#Todo Formatting of display(Look up lab fibonacci program(sir's solution))
+    print("Restaurant Name\t\t\t\t\t","Average Price")#Print every restaurant name and the restaurant's average price
+    for i in range(len(averrest)):
+        print(averrest[i][0],"\t\t\t\t\t",averrest[i][1])
+    restname = input("Enter which restaurant you would like to choose:")#Asks the user to choose a restaurant
+    menu = restdict[restname]
+    print("Dishes\t\t","Price")
+    for i in menu:#Prints the menu of the restaurant chosen by the user
+        print(i[0],"\t\t",i[1])
 
-userfoods = userinput()
 restdict = getdata()
-#todo Have to compare total menu price for every restaurant not every food(Change that)
-for userfood in userfoods:#A loop to get the lowest price for each and every product added to the cart
-    userfoodinrests = finditem(userfood)
-    lowestprice(userfoodinrests)
-
+averrest = cheaprest(restdict)
+dispavg(averrest,restdict)
+#Todo Accept User input add to cart and then provide total price and an option to view the order
