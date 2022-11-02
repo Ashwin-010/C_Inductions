56ed64b47ffef0ef97bf68166f3a7cc71352a3b9
Esh-010 <109636159+Ashwin-010@users.noreply.github.com>
2022-11-02 19:01:26 +0530

    Added Function to add items to cart and view cart

--- Project New/Python File/Code.py
+++ Project New/Python File/Code.py
@@ -45,3 +45,36 @@ restdict = getdata()
 averrest = cheaprest(restdict)
 dispavg(averrest,restdict)
 #Todo Accept User input add to cart and then provide total price and an option to view the order
+
+def addtocart(restdict):#function to add items to cart
+    cart = {}
+    while True:
+        restchoice = dispavg(averrest, restdict)#restaurant from which they would like item
+        foodchoice = int(input("Enter Item Number of food item you would like to add: "))#indec of item they choose
+        quantity = int(input("Enter quantity you would like to order: "))#quantity of item
+        menu = restdict[restchoice]         #next 6 lines are for getting item price
+        for items in menu:
+            if items[0] == restdict[restchoice][foodchoice-1][0]:
+                itemprice = int(items[-1])
+                price = itemprice
+        for i in restdict:#adding item to cart
+            if i==restchoice:
+                cart[restdict[i][foodchoice-1][0]]= (restchoice,price,quantity)
+        yorn = input("Would you like to add another item(y/n)? ")
+        if yorn.lower()=='n':
+            break
+    return cart#returning dictionary containing item name,restaurant ordered from,price and quantity
+
+
+def viewcart(cart):#function to view items in cart
+    print("S.No\tItem\tRestaurant\tQuantity\tprice")
+    total = 0
+    serialno = 1
+    for i in cart:
+        print(serialno,".\t",i,"\t",cart[i][0],"\t",cart[i][-1],"\t",(cart[i][1])*2)
+        total+=((cart[i][1])*2)
+        serialno+=1
+    print("Total = ",total)
+    print("GST = 18%")
+    print("Grand Total = ",total*0.18)
+viewcart(addtocart(restdict))
